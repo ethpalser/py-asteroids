0d6b3aafdcc422d9364fda1b43027ce6e8564a99
ethpalser <eth.palser@gmail.com>
2024-08-26 18:31:14 -0400

    created player on screen

--- main.py
+++ main.py
@@ -3,18 +3,21 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     init_result = pygame.init()
     clock = pygame.time.Clock()
     dt = 0 # delta time
 
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color = (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         time_since = clock.tick(60) # pause until 1/60 of second passes
         dt = time_since / 1000
